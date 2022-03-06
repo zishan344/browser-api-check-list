2e8ac15b5f5430364839c9683eca6a58772ef833
zishan344 <zishanahmed344@gmail.com>
2022-03-06 11:09:12 +0600

    done the problem number two

---
+++ problem2.js
@@ -0,0 +1,6 @@
+/* ২. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০ যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও  */
+function promptValue() {
+  const prompti = Number(prompt("enter the value")) + 200;
+  return alert(prompti);
+}
+promptValue();
